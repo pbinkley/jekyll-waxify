4f33a997a092a6ef63ba96994e4e54d5694fa8df
Peter Binkley <peter.binkley@ualberta.ca>
2021-03-25 11:33:16 -0600

    Gem skeleton created with bundle

---
+++ lib/jekyll/waxify.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "waxify/version"
+
+module Jekyll
+  module Waxify
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/waxify/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Waxify
+    VERSION = "0.1.0"
+  end
+end
